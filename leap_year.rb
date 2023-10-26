eb52c5257b549040019fb9b9adc5b18b69119c8f
Tracy Cano <tracycano3.14@gmail.com>
2023-10-26 11:55:16 -0700

    code minification for if statements for leap year program

--- leap_year.rb
+++ leap_year.rb
@@ -1,12 +1,5 @@
 def is_leap_year(year)
-    if (year % 4 == 0)
-        if (year % 100 == 0)
-          return (year % 400 == 0)
-        else
-          return true
-        end
-      end
-    return false 
+  (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
   end
   
   print "Enter a start year: "
